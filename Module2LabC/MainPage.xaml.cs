bc09b204ebd2d9fd21fe5f7907cf6d646a45e7af
ashstan <ashleystanley.jb@gmail.com>
2023-04-13 19:51:33 -0500

    Add project files.

---
+++ Module2LabC/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module2LabC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string code = "";
+        string message = "";
+
+        void DotClicked(object sender, EventArgs e)
+        {
+            code += ".";
+            output_code.Text = code;
+        }
+
+        void DashClicked(object sender, EventArgs e)
+        {
+            code += "-";
+            output_code.Text = code;
+        }
+
+        void SpaceClicked(object sender, EventArgs e)
+        {
+            message += Morse.MorseCoder(code);
+            output.Text = message;
+            code = "";
+            output_code.Text = code;
+        }
+    }
+}
